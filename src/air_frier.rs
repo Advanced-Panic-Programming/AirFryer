799dbfa33a2d6f60981d0de45365791d43264f5b
Davide Benetti <davide.benetti@studenti.unitn.it>
2025-12-01 16:47:33 +0100

    First Steps

--- src/air_frier.rs
+++ src/air_frier.rs
@@ -1,6 +1,7 @@
+use std::collections::HashSet;
 use common_game::components::planet;
 use common_game::components::planet::{PlanetState, PlanetType};
-use common_game::components::resource::{Combinator, Generator};
+use common_game::components::resource::{BasicResourceType, Combinator, ComplexResourceType, Generator};
 use common_game::components::rocket::Rocket;
 use common_game::protocols::messages::{ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator};
 
@@ -19,12 +20,7 @@ impl planet::PlanetAI for PlanetAI {
                 }
             }
             OrchestratorToPlanet::Asteroid(_) => {
-                if state.has_rocket() {
-                    Some(PlanetToOrchestrator::AsteroidAck { planet_id: state.id(), rocket: state.take_rocket() })
-                }
-                else {
-                    Some(PlanetToOrchestrator::AsteroidAck { planet_id: state.id(), rocket: None })
-                }
+                Some(PlanetToOrchestrator::AsteroidAck {planet_id: state.id(), rocket: self.handle_asteroid(state, generator, combinator)})
             }
             OrchestratorToPlanet::StartPlanetAI(_) => {
                 todo!()
@@ -39,11 +35,45 @@ impl planet::PlanetAI for PlanetAI {
     }
 
     fn handle_explorer_msg(&mut self, state: &mut PlanetState, generator: &Generator, combinator: &Combinator, msg: ExplorerToPlanet) -> Option<PlanetToExplorer> {
-        todo!()
+        match msg {
+            ExplorerToPlanet::SupportedResourceRequest {..} => {
+                let mut hs = HashSet::new();
+                hs.insert(BasicResourceType::Carbon);
+                Some(PlanetToExplorer::SupportedResourceResponse { resource_list: Some( hs) })
+            }
+            ExplorerToPlanet::SupportedCombinationRequest { .. } => {
+                let mut hs = HashSet::new();
+                hs.insert(ComplexResourceType::AIPartner);
+                hs.insert(ComplexResourceType::Diamond);
+                hs.insert(ComplexResourceType::Dolphin);
+                hs.insert(ComplexResourceType::Water);
+                hs.insert(ComplexResourceType::Life);
+                hs.insert(ComplexResourceType::Robot);
+                Some(PlanetToExplorer::SupportedCombinationResponse { combination_list: Some(hs),});
+                todo!()
+            }
+            ExplorerToPlanet::GenerateResourceRequest { .. } => {
+                todo!()
+            }
+            ExplorerToPlanet::CombineResourceRequest { .. } => {
+                todo!()
+            }
+            ExplorerToPlanet::AvailableEnergyCellRequest { .. } => {
+                todo!()
+            }
+            ExplorerToPlanet::InternalStateRequest { .. } => {
+                todo!()
+            }
+        }
     }
 
     fn handle_asteroid(&mut self, state: &mut PlanetState, generator: &Generator, combinator: &Combinator) -> Option<Rocket> {
-        todo!()
+        if state.has_rocket() {
+            Some(state.take_rocket()?)
+        }
+        else{
+            None
+        }
     }
 
     fn start(&mut self, state: &PlanetState) {
