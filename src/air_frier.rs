9d057431cbf320506a2c13fe3750f255c6cadd51
Davide Benetti <davide.benetti@studenti.unitn.it>
2025-12-02 16:26:56 +0100

    Ulteriori add

--- src/air_frier.rs
+++ src/air_frier.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::os::macos::raw::stat;
 use common_game::components::planet;
 use common_game::components::planet::{PlanetState, PlanetType};
 use common_game::components::resource::{BasicResourceType, Combinator, ComplexResourceType, Generator};
@@ -6,10 +7,13 @@ use common_game::components::rocket::Rocket;
 use common_game::protocols::messages::{ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator};
 
 pub struct PlanetAI{
+    has_planet : bool,
 }
 impl PlanetAI {
     pub fn new() -> PlanetAI {
-        PlanetAI {}
+        PlanetAI {
+            has_planet : false,
+        }
     }
 }
 impl planet::PlanetAI for PlanetAI {
@@ -27,10 +31,12 @@ impl planet::PlanetAI for PlanetAI {
                 Some(PlanetToOrchestrator::AsteroidAck {planet_id: state.id(), rocket: self.handle_asteroid(state, generator, combinator)})
             }
             OrchestratorToPlanet::StartPlanetAI(_) => {
-                todo!()
+                self.start(state);
+                Some(PlanetToOrchestrator::StartPlanetAIResult {planet_id: state.id(),timestamp: todo!()})
             }
             OrchestratorToPlanet::StopPlanetAI(_) => {
-                todo!()
+                self.stop(state);
+                Some(PlanetToOrchestrator::StopPlanetAIResult {planet_id: state.id(), timestamp: todo!()})
             }
             OrchestratorToPlanet::InternalStateRequest(_) => {
                 todo!()
@@ -80,6 +86,8 @@ impl planet::PlanetAI for PlanetAI {
     }
 
     fn start(&mut self, state: &PlanetState) {
+        //Manda messaggio per vedere se ha l'esploratore
+        //if -> true o false
         todo!()
     }
 
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,24 @@ use common_game::components::planet::{Planet, PlanetType};
 use common_game::components::resource::{BasicResourceType, ComplexResourceType};
 use common_game::protocols::messages::{ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator};
 fn main() {
+    //New AI
     let ia = air_frier::PlanetAI::new();
+
     let mut gene:Vec<BasicResourceType> = Vec::new();
     gene.push(BasicResourceType::Carbon);
+
     let mut compl:Vec<ComplexResourceType> = Vec::new();
     compl.push(ComplexResourceType::Water);
     compl.push(ComplexResourceType::Life);
     compl.push(ComplexResourceType::Dolphin);
     compl.push(ComplexResourceType::Robot);
     compl.push(ComplexResourceType::AIPartner);
+
     let (sdr_expl_to_planet, rcv_expl_to_planet) = mpsc::channel::<ExplorerToPlanet>();
     let (sdr_planet_to_expl, rcv_planet_to_expl) = mpsc::channel::<PlanetToExplorer>();
     let (sdr_planet_to_orc, rcv_planet_to_orc) = mpsc::channel::<PlanetToOrchestrator>();
     let (sdr_orc_to_planet, rcv_orc_to_planet) = mpsc::channel::<OrchestratorToPlanet>();
+
     let planet = Planet::new(0, PlanetType::C, ia, gene, compl, (rcv_orc_to_planet, sdr_planet_to_orc), (rcv_expl_to_planet, sdr_planet_to_expl));
     if planet.is_ok(){
         planet.unwrap().run();
@@ -25,3 +30,12 @@ fn main() {
     //Planet::new(0, PlanetType::C, (), vec![], vec![], ((), ()), ((), ()));
 
 }
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+
+    }
+}
+
+
