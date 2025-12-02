558c52657aa59fdb39854bb3a2cef53c0ac8ad29
Davide Benetti <davide.benetti@studenti.unitn.it>
2025-12-02 17:28:19 +0100

    Test poco test

--- src/air_frier.rs
+++ src/air_frier.rs
@@ -1,46 +1,52 @@
 use std::collections::HashSet;
-use std::os::macos::raw::stat;
 use common_game::components::planet;
 use common_game::components::planet::{PlanetState, PlanetType};
-use common_game::components::resource::{BasicResourceType, Combinator, ComplexResourceType, Generator};
+use common_game::components::resource::{BasicResource, BasicResourceType, Combinator, ComplexResourceType, Generator};
 use common_game::components::rocket::Rocket;
 use common_game::protocols::messages::{ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator};
 
 pub struct PlanetAI{
-    has_planet : bool,
+    has_explorer : bool,
+    started: bool,
 }
 impl PlanetAI {
     pub fn new() -> PlanetAI {
         PlanetAI {
-            has_planet : false,
+            has_explorer : false,
+            started: false,
         }
     }
 }
 impl planet::PlanetAI for PlanetAI {
     fn handle_orchestrator_msg(&mut self, state: &mut PlanetState, generator: &Generator, combinator: &Combinator, msg: OrchestratorToPlanet) -> Option<PlanetToOrchestrator> {
-        match msg {
-            OrchestratorToPlanet::Sunray(_) => {
-                if state.has_rocket() {
+        if self.started {
+            match msg {
+                OrchestratorToPlanet::Sunray(_) => {
+                    if state.has_rocket() {
+                        todo!()
+                    }
+                    else {
+                        todo!()
+                    }
+                }
+                OrchestratorToPlanet::Asteroid(_) => {
+                    Some(PlanetToOrchestrator::AsteroidAck {planet_id: state.id(), rocket: self.handle_asteroid(state, generator, combinator)})
+                }
+                OrchestratorToPlanet::StartPlanetAI(_) => {
+                    self.start(state);
+                    Some(PlanetToOrchestrator::StartPlanetAIResult {planet_id: state.id(),timestamp: todo!()})
+                }
+                OrchestratorToPlanet::StopPlanetAI(_) => {
+                    self.stop(state);
+                    Some(PlanetToOrchestrator::StopPlanetAIResult {planet_id: state.id(), timestamp: todo!()})
+                }
+                OrchestratorToPlanet::InternalStateRequest(_) => {
                     todo!()
                 }
-                else {
-                    todo!()
-                }
             }
-            OrchestratorToPlanet::Asteroid(_) => {
-                Some(PlanetToOrchestrator::AsteroidAck {planet_id: state.id(), rocket: self.handle_asteroid(state, generator, combinator)})
-            }
-            OrchestratorToPlanet::StartPlanetAI(_) => {
-                self.start(state);
-                Some(PlanetToOrchestrator::StartPlanetAIResult {planet_id: state.id(),timestamp: todo!()})
-            }
-            OrchestratorToPlanet::StopPlanetAI(_) => {
-                self.stop(state);
-                Some(PlanetToOrchestrator::StopPlanetAIResult {planet_id: state.id(), timestamp: todo!()})
-            }
-            OrchestratorToPlanet::InternalStateRequest(_) => {
-                todo!()
-            }
+        }
+        else{
+            None
         }
     }
 
@@ -61,8 +67,22 @@ impl planet::PlanetAI for PlanetAI {
                 hs.insert(ComplexResourceType::Robot);
                 Some(PlanetToExplorer::SupportedCombinationResponse { combination_list: Some(hs)})
             }
-            ExplorerToPlanet::GenerateResourceRequest { .. } => {
-                todo!()
+            ExplorerToPlanet::GenerateResourceRequest {explorer_id, resource} => {
+                if resource != BasicResourceType::Carbon {
+                    None
+                }
+                else{
+                    let carbon = generator.make_carbon(state.cell_mut(0));
+                    match carbon {
+
+                        Ok(res) => {
+
+                            Some(PlanetToExplorer::GenerateResourceResponse {resource: Some(BasicResource::Carbon(res))})
+                        }
+                        Err(_) => {None}
+                    }
+
+                }
             }
             ExplorerToPlanet::CombineResourceRequest { .. } => {
                 todo!()
@@ -88,10 +108,12 @@ impl planet::PlanetAI for PlanetAI {
     fn start(&mut self, state: &PlanetState) {
         //Manda messaggio per vedere se ha l'esploratore
         //if -> true o false
-        todo!()
+        self.started = true;
+        //to do
     }
 
     fn stop(&mut self, state: &PlanetState) {
-        todo!()
+        self.started = false;
+        //to do
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -32,9 +32,47 @@ fn main() {
 }
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
+    use std::os::macos::raw::pthread_t;
+    use std::sync::mpsc;
+    use std::thread;
+    use common_game::components::planet::{Planet, PlanetType};
+    use common_game::components::resource::{BasicResource, BasicResourceType, Carbon, ComplexResourceType};
+    use common_game::protocols::messages::{ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator, StartPlanetAiMsg};
+    use crate::air_frier;
 
+    #[test]
+    fn ask_for_carbon_from_explorer() {
+        let ia = air_frier::PlanetAI::new();
+
+        let mut gene:Vec<BasicResourceType> = Vec::new();
+        gene.push(BasicResourceType::Carbon);
+
+        let mut compl:Vec<ComplexResourceType> = Vec::new();
+        compl.push(ComplexResourceType::Water);
+        compl.push(ComplexResourceType::Life);
+        compl.push(ComplexResourceType::Dolphin);
+        compl.push(ComplexResourceType::Robot);
+        compl.push(ComplexResourceType::AIPartner);
+        let (sdr_expl_to_planet, rcv_expl_to_planet) = mpsc::channel::<ExplorerToPlanet>();
+        let (sdr_planet_to_expl, rcv_planet_to_expl) = mpsc::channel::<PlanetToExplorer>();
+        let (sdr_planet_to_orc, rcv_planet_to_orc) = mpsc::channel::<PlanetToOrchestrator>();
+        let (sdr_orc_to_planet, rcv_orc_to_planet) = mpsc::channel::<OrchestratorToPlanet>();
+
+        let planet = Planet::new(0, PlanetType::C, ia, gene, compl, (rcv_orc_to_planet, sdr_planet_to_orc), (rcv_expl_to_planet, sdr_planet_to_expl));
+        sdr_orc_to_planet.send(OrchestratorToPlanet::StartPlanetAI(StartPlanetAiMsg));
+        sdr_expl_to_planet.send(ExplorerToPlanet::GenerateResourceRequest {explorer_id: 0, resource: BasicResourceType::Carbon});
+        planet.unwrap().run().expect("TODO: panic message");
+        let t1 = thread::spawn(move ||{
+            let res = rcv_planet_to_expl.recv();
+            match res{
+                Ok(msg) => {
+                    println!("Result OK");
+                }
+                Err(_) => {
+                    assert_eq!(1,2);
+                }
+            }
+        });
     }
 }
 
