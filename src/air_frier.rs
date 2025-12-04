2d549ae30bed87ee1269c2324195ba82ff6b6d92
daniele.mattuzzi <ildanimail@gmail.com>
2025-12-04 14:16:40 +0100

    Added CI configuration + .gitignore
    Removed target dir
    
    Also formatted the code

--- src/air_frier.rs
+++ src/air_frier.rs
@@ -1,81 +1,102 @@
+use common_game::components::planet;
+use common_game::components::planet::{PlanetState, PlanetType};
+use common_game::components::resource::{
+    BasicResource, BasicResourceType, Combinator, ComplexResourceType, Generator,
+};
+use common_game::components::rocket::Rocket;
+use common_game::components::sunray::Sunray;
+use common_game::protocols::messages::{
+    ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator,
+};
 use std::collections::HashSet;
 use std::os::unix::raw::time_t;
 use std::time::SystemTime;
-use common_game::components::planet;
-use common_game::components::planet::{PlanetState, PlanetType};
-use common_game::components::resource::{BasicResource, BasicResourceType, Combinator, ComplexResourceType, Generator};
-use common_game::components::rocket::Rocket;
-use common_game::components::sunray::Sunray;
-use common_game::protocols::messages::{ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator};
-pub struct PlanetAI{
-    has_explorer : bool,
+pub struct PlanetAI {
+    has_explorer: bool,
     started: bool,
 }
 impl PlanetAI {
     pub fn new() -> PlanetAI {
         PlanetAI {
-            has_explorer : false,
+            has_explorer: false,
             started: false,
         }
     }
 }
 impl planet::PlanetAI for PlanetAI {
-    fn handle_orchestrator_msg(&mut self, state: &mut PlanetState, generator: &Generator, combinator: &Combinator, msg: OrchestratorToPlanet) -> Option<PlanetToOrchestrator> {
+    fn handle_orchestrator_msg(
+        &mut self,
+        state: &mut PlanetState,
+        generator: &Generator,
+        combinator: &Combinator,
+        msg: OrchestratorToPlanet,
+    ) -> Option<PlanetToOrchestrator> {
         if self.started {
             match msg {
                 OrchestratorToPlanet::Sunray(Sunray) => {
-                    if ! state.cell(0).is_charged() {
-
+                    if !state.cell(0).is_charged() {
                         state.cell_mut(0).charge(Sunray);
 
-                        if state.can_have_rocket(){
-
-                            if ! state.has_rocket(){
+                        if state.can_have_rocket() {
+                            if !state.has_rocket() {
                                 state.build_rocket(0);
                             }
                         }
-                    }
-                    else{
+                    } else {
                         state.build_rocket(0);
                         state.cell_mut(0).charge(Sunray);
                     }
-                    Some(PlanetToOrchestrator::SunrayAck { planet_id: 0})
+                    Some(PlanetToOrchestrator::SunrayAck { planet_id: 0 })
                 }
                 OrchestratorToPlanet::Asteroid(Asteroid) => {
-                    Some(PlanetToOrchestrator::AsteroidAck {planet_id: state.id(), rocket: self.handle_asteroid(state, generator, combinator)})
+                    Some(PlanetToOrchestrator::AsteroidAck {
+                        planet_id: state.id(),
+                        rocket: self.handle_asteroid(state, generator, combinator),
+                    })
                 }
                 OrchestratorToPlanet::StartPlanetAI => {
                     self.start(state);
-                    Some(PlanetToOrchestrator::StartPlanetAIResult {planet_id: state.id()})
+                    Some(PlanetToOrchestrator::StartPlanetAIResult {
+                        planet_id: state.id(),
+                    })
                 }
                 OrchestratorToPlanet::StopPlanetAI => {
                     self.stop(state);
-                    Some(PlanetToOrchestrator::StopPlanetAIResult {planet_id: state.id()})
+                    Some(PlanetToOrchestrator::StopPlanetAIResult {
+                        planet_id: state.id(),
+                    })
                 }
                 OrchestratorToPlanet::InternalStateRequest => {
                     todo!()
-                },
-                OrchestratorToPlanet::IncomingExplorerRequest { .. } =>{
+                }
+                OrchestratorToPlanet::IncomingExplorerRequest { .. } => {
                     todo!()
                 }
                 OrchestratorToPlanet::OutgoingExplorerRequest { .. } => {
                     todo!()
                 }
             }
-        }
-        else{
+        } else {
             None
         }
     }
 
-    fn handle_explorer_msg(&mut self, state: &mut PlanetState, generator: &Generator, combinator: &Combinator, msg: ExplorerToPlanet) -> Option<PlanetToExplorer> {
+    fn handle_explorer_msg(
+        &mut self,
+        state: &mut PlanetState,
+        generator: &Generator,
+        combinator: &Combinator,
+        msg: ExplorerToPlanet,
+    ) -> Option<PlanetToExplorer> {
         match msg {
-            ExplorerToPlanet::SupportedResourceRequest {explorer_id } => {
+            ExplorerToPlanet::SupportedResourceRequest { explorer_id } => {
                 let mut hs = HashSet::new();
                 hs.insert(BasicResourceType::Carbon);
-                Some(PlanetToExplorer::SupportedResourceResponse { resource_list: Some( hs) })
+                Some(PlanetToExplorer::SupportedResourceResponse {
+                    resource_list: Some(hs),
+                })
             }
-            ExplorerToPlanet::SupportedCombinationRequest {explorer_id} => {
+            ExplorerToPlanet::SupportedCombinationRequest { explorer_id } => {
                 let mut hs = HashSet::new();
                 hs.insert(ComplexResourceType::AIPartner);
                 hs.insert(ComplexResourceType::Diamond);
@@ -83,45 +104,54 @@ impl planet::PlanetAI for PlanetAI {
                 hs.insert(ComplexResourceType::Water);
                 hs.insert(ComplexResourceType::Life);
                 hs.insert(ComplexResourceType::Robot);
-                Some(PlanetToExplorer::SupportedCombinationResponse { combination_list: Some(hs)})
+                Some(PlanetToExplorer::SupportedCombinationResponse {
+                    combination_list: Some(hs),
+                })
             }
-            ExplorerToPlanet::GenerateResourceRequest {explorer_id, resource} => {
+            ExplorerToPlanet::GenerateResourceRequest {
+                explorer_id,
+                resource,
+            } => {
                 if resource != BasicResourceType::Carbon {
-                    Some(PlanetToExplorer::GenerateResourceResponse {resource: None})
-                }
-                else{
+                    Some(PlanetToExplorer::GenerateResourceResponse { resource: None })
+                } else {
                     let generated = generator.make_carbon(state.cell_mut(0));
                     match generated {
-                        Ok(carbon) => {
-                            Some(PlanetToExplorer::GenerateResourceResponse {resource: Some(BasicResource::Carbon(carbon))})
-                        }
+                        Ok(carbon) => Some(PlanetToExplorer::GenerateResourceResponse {
+                            resource: Some(BasicResource::Carbon(carbon)),
+                        }),
                         Err(string) => {
                             Some(PlanetToExplorer::GenerateResourceResponse { resource: None })
                         }
                     }
                 }
-
             }
             ExplorerToPlanet::CombineResourceRequest { .. } => {
                 todo!()
             }
             ExplorerToPlanet::AvailableEnergyCellRequest { .. } => {
-                Some(PlanetToExplorer::AvailableEnergyCellResponse { available_cells: state.cells_count() as u32 })
+                Some(PlanetToExplorer::AvailableEnergyCellResponse {
+                    available_cells: state.cells_count() as u32,
+                })
             }
         }
     }
 
-    fn handle_asteroid(&mut self, state: &mut PlanetState, generator: &Generator, combinator: &Combinator) -> Option<Rocket> {
+    fn handle_asteroid(
+        &mut self,
+        state: &mut PlanetState,
+        generator: &Generator,
+        combinator: &Combinator,
+    ) -> Option<Rocket> {
         if state.has_rocket() {
             Some(state.take_rocket()?)
-        }  else {
+        } else {
             // Error handling is done in the common-code trait implementation
             // i.e. planet can have rocket, planet energy cell is charged, ...
 
             // Try to build the rocket
             let _ = state.build_rocket(state.cells_count());
             state.take_rocket()
-
         }
     }
 
