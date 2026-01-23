246e53831dbd38f71ac097664c8c9e9be092c4e3
danielemattuzzi <danielemattuzzi@users.noreply.github.com>
2026-01-23 20:31:38 +0000

    deploy: 6b9376eabdfbfeccd53bd6199223ef228a03853a

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["air_fryer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"air_fryer/struct.PlanetAI.html\" title=\"struct air_fryer::PlanetAI\">PlanetAI</a>"]]]]);
+    var implementors = Object.fromEntries([["air_fryer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"air_fryer/struct.PlanetAI.html\" title=\"struct air_fryer::PlanetAI\">PlanetAI</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
