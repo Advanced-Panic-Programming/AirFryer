1a574db185f081ced0ed542904e9e835136289ef
danielemattuzzi <danielemattuzzi@users.noreply.github.com>
2026-07-02 20:39:36 +0000

    deploy: 737ca4d2c949f6c746e586e5e29d876f14e7fab9

--- trait.impl/core/marker/trait.Sync.js
+++ trait.impl/core/marker/trait.Sync.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["air_fryer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"air_fryer/struct.PlanetAI.html\" title=\"struct air_fryer::PlanetAI\">PlanetAI</a>",0,1,["air_fryer::planet::PlanetAI"]]]]]);
+    const implementors = Object.fromEntries([["air_fryer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"air_fryer/struct.PlanetAI.html\" title=\"struct air_fryer::PlanetAI\">PlanetAI</a>",0,1,["air_fryer::planet::PlanetAI"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/marker/trait.Unpin.js
+++ trait.impl/core/marker/trait.Unpin.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["air_fryer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"air_fryer/struct.PlanetAI.html\" title=\"struct air_fryer::PlanetAI\">PlanetAI</a>",0,1,["air_fryer::planet::PlanetAI"]]]]]);
+    const implementors = Object.fromEntries([["air_fryer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"air_fryer/struct.PlanetAI.html\" title=\"struct air_fryer::PlanetAI\">PlanetAI</a>",0,1,["air_fryer::planet::PlanetAI"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
